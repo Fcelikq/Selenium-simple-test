bfb178968499f3764e1cc47ae25121b9a093e473
Fırat Çelik <49812357+Fcelikq@users.noreply.github.com>
2019-12-04 22:43:48 +0300

    Selenium projesi eklendi

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.n11.com/");
+            driver.FindElement(By.Id("searchData")).SendKeys("iphone");
+            driver.FindElement(By.ClassName("icon")).Click();
+        }
+    }
+}
